31807d83083e90f0698520325e6a49ee094b721a
unknown <marclacasa17@gmail.com>
2025-09-28 00:54:19 +0200

    Literally only removed 1 line

--- 2015/2.dayTwo/dayTwo.py
+++ 2015/2.dayTwo/dayTwo.py
@@ -2,9 +2,10 @@ result = 0
 
 with open("dayTwo.txt", "r") as f:
     for line in f:
-        line = line.strip()
-        l, w, h = map(int, line.split("x"))
+        l, w, h = map(int, line.strip().split("x"))
 
         result += (2*l*w + 2*w*h + 2*h*l) + (min((l*w),(w*h),(h*l)))
 
 print(result)
+
+#1606483
