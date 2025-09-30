65bc7a4d45a75ab9281fc13ae9218cb81ec2a30f
unknown <marclacasa17@gmail.com>
2025-09-30 12:09:27 +0200

    Added the structure of day 5

---
+++ 2015/7.daySeven/daySeven.py
@@ -0,0 +1,4 @@
+# The instructions to this file are not in order, i need to change this program so that it first saves each instruction and then executes them in the order needed
+
+
+#It may be a good idea to delete the entire code and start again
