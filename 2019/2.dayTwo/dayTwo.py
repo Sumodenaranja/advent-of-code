56539c714fa7c021147da7b71d692edc574561d1
Sumodenaranja <marclacasa17@gmail.com>
2025-10-13 20:44:19 +0200

    Grab possible errors

--- 2019/2.dayTwo/dayTwo.py
+++ 2019/2.dayTwo/dayTwo.py
@@ -14,6 +14,8 @@ while i < len(numbers):
     elif numbers[i] == 99:
         print("Halt!")
         break
+    else:
+        raise Exception("The instruction is not 1,2 or 99")
 
     i += 4 # 4 Because the blocks are made of 4 instructions
 
