276b60f7dc9bb8125fb131766bbb93318d4670ec
unknown <marclacasa17@gmail.com>
2025-10-17 23:58:08 +0200

    Made the code more readable

--- 2024/2.dayTwo/dayTwo.py
+++ 2024/2.dayTwo/dayTwo.py
@@ -1,30 +1,20 @@
-import time
-
-start_time = time.time()
-
 result = 0
 
-with open("dayTwo.txt","r") as f:
+with open("dayTwo.txt", "r") as f:
     for line in f:
         sum = True
-        data = tuple(map(int,line.strip().split()))
-        if tuple(sorted(data)) == data or tuple(sorted(data)) == data[::-1]:
-            n = data[0]
-            for number in data[1:]:
-                if number == n or abs(number-n) > 3:
+
+        values = list(map(int,line.strip().split()))
+
+        if sorted(values) == values or sorted(values) == values[::-1]:
+            for v1, v2 in zip(values[:-1],values[1:]):
+
+                if (abs(v1-v2) > 3) or (v1 == v2):
                     sum = False
                     break
-                n = number
+            
             if sum:
                 result += 1
-        else:
-            continue
-
-            
 
 print(result)
-print("Total execution time was: " + str(time.time()-start_time))
-
-
-# 224
-# Correct :)
+            
